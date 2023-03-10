de40a9fd0d45bea485dd0e064b0176451a8ae908
zeotoni <zeotoni666@gmail.com>
2023-03-09 23:15:17 -0300

    finalizando pagina ingresso comprado

--- scripts/menu.js
+++ scripts/menu.js
@@ -5,3 +5,10 @@ btnMenu.onclick = () => {
     menuMobile.classList.toggle('hidden');
     btnMenu.dataset.btnMenu == "fechar" ? btnMenu.dataset.btnMenu = "" : btnMenu.dataset.btnMenu = "fechar";
 }
+
+
+const seta = document.querySelector('[data-arrow]');
+const p = document.querySelector('.perguntas__frequentes-resposta')
+seta.onclick = () => {
+    p.classList.toggle('hidden')
+}
