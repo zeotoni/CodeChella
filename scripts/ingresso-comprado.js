8bccac183ab2cb7c9bc8daa21a0a90a2177a73ec
zeotoni <zeotoni666@gmail.com>
2023-03-16 19:15:30 -0300

    finalizando ingresso

--- scripts/ingresso-comprado.js
+++ scripts/ingresso-comprado.js
@@ -6,7 +6,7 @@ const exibeIngresso = () => {
     const dados = JSON.parse(localStorage.getItem('dados'))
     divIngresso.innerHTML = `
     <h2 class="ingresso__card-info-nome">${dados.nome}</h2>
-    <p class="ingresso__card-info-tipo-ingresso">Tipo de ingresso: ${dados.ingresso}</p>
+    <p class="ingresso__card-info-tipo-ingresso">Tipo de entrada: ${dados.ingresso}</p>
     <p class="ingresso__card-info-tipo-ingresso">Setor: ${dados.setor}</p>
     <p class="ingresso__card-info-data">Data: ${dados.dataEvento}</p>
     <p class="ingresso__card-info-local">Local: São Paulo-SP</p>
